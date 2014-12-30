1dee1b3873259ce4e07cb488cb3af484e7a5e677
Daniel Ma <drailskid@yahoo.com>
2014-12-29 23:18:48 -0800

    Initial commit of foundation apps starter

---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+// FOUNDATION FOR APPS TEMPLATE GULPFILE
+// -------------------------------------
+// This file processes all of the assets in the "client" folder, combines them with the Foundation
+// for Apps assets, and outputs the finished files in the "build" folder as a finished app.
+
+// 1. LIBRARIES
+// - - - - - - - - - - - - - - -
+
+var gulp           = require('gulp'),
+    rimraf         = require('rimraf'),
+    runSequence    = require('run-sequence'),
+    autoprefixer   = require('gulp-autoprefixer'),
+    sass           = require('gulp-ruby-sass'),
+    uglify         = require('gulp-uglify'),
+    concat         = require('gulp-concat'),
+    connect        = require('gulp-connect'),
+    path           = require('path');
+
+// 2. SETTINGS VARIABLES
+// - - - - - - - - - - - - - - -
+
+// Sass will check these folders for files when you use @import.
+var sassPaths = [
+  'client/assets/scss',
+  'bower_components/foundation-apps/scss',
+  'bower_components/font-awesome/scss'
+];
+// These files include Foundation for Apps and its dependencies
+var foundationJS = [
+  'bower_components/fastclick/lib/fastclick.js',
+  'bower_components/viewport-units-buggyfill/viewport-units-buggyfill.js',
+  'bower_components/tether/tether.js',
+];
+// These files are for your app's JavaScript
+var appJS = [
+  'client/assets/js/app.js'
+];
+
+// 3. TASKS
+// - - - - - - - - - - - - - - -
+
+// Cleans the build directory
+gulp.task('clean', function(cb) {
+  rimraf('./build', cb);
+});
+
+// Copies user-created files and Foundation assets
+gulp.task('copy', function() {
+  var dirs = [
+    './client/**/*.*',
+    '!./client/assets/{scss,js}/**/*.*'
+  ];
+
+  // Everything in the client folder except templates, Sass, and JS
+  gulp.src(dirs, {
+    base: './client/'
+  })
+    .pipe(gulp.dest('./build'));
+});
+
+// Compiles Sass
+gulp.task('sass', function() {
+  return gulp.src('client/assets/scss/app.scss')
+    .pipe(sass({
+      loadPath: sassPaths,
+      style: 'nested',
+      bundleExec: true,
+      'sourcemap=none': true
+    }))
+    .on('error', function(e) {
+      console.log(e);
+    })
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie 10']
+    }))
+    .pipe(gulp.dest('./build/assets/css/'));
+});
+
+gulp.task('icons', function() {
+  return gulp.src('bower_components/font-awesome/fonts/**.*')
+    .pipe(gulp.dest('./build/assets/fonts/'));
+});
+
+// Compiles and copies the Foundation for Apps JavaScript, as well as your app's custom JS
+gulp.task('uglify', function() {
+  // Foundation JavaScript
+  gulp.src(foundationJS)
+    .pipe(uglify({
+      beautify: true,
+      mangle: false
+    }).on('error', function(e) {
+      console.log(e);
+    }))
+    .pipe(concat('foundation.js'))
+    .pipe(gulp.dest('./build/assets/js/'))
+  ;
+
+  // App JavaScript
+  return gulp.src(appJS)
+    .pipe(uglify({
+      beautify: true,
+      mangle: false
+    }).on('error', function(e) {
+      console.log(e);
+    }))
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./build/assets/js/'))
+  ;
+});
+
+// Starts a test server, which you can view at http://localhost:8080
+gulp.task('server:start', function() {
+  connect.server({
+    root: './build'
+  });
+});
+
+// Builds your entire app once, without starting a server
+gulp.task('build', function() {
+  runSequence('clean', ['copy', 'sass', 'icons', 'uglify'], function() {
+    console.log("Successfully built.");
+  })
+});
+
+// Default task: builds your app, starts a server, and recompiles assets when they change
+gulp.task('default', ['build', 'server:start'], function() {
+  // Watch Sass
+  gulp.watch(['./client/assets/scss/**/*', './scss/**/*'], ['sass']);
+
+  // Watch JavaScript
+  gulp.watch(['./client/assets/js/**/*', './js/**/*'], ['uglify']);
+
+  // Watch static files
+  gulp.watch(['./client/**/*.*', '!./client/templates/**/*.*', '!./client/assets/{scss,js}/**/*.*'], ['copy']);
+});
