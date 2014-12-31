3b378451653202382e8e404c960d4a431389267e
Daniel Ma <drailskid@yahoo.com>
2014-12-31 13:34:34 -0800

    remove jQuery (for now)

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,6 @@ var vendorJS = [
   'bower_components/viewport-units-buggyfill/viewport-units-buggyfill.js',
   'bower_components/tether/tether.js',
   'bower_components/lodash/dist/lodash.min.js',
-  'bower_components/jquery/dist/jquery.min.js'
 ];
 
 var appCoffee = [
